d5872ab448209dd8545e24c657dad6f17a4d5b3f
sangeethck <sangeethck7@gmail.com>
2022-10-11 20:23:05 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   lib/main.dart
    	modified:   lib/splash/splash_screen.dart

--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -9,14 +9,24 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final hight = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset('assets/images/Digital transformation-bro.png'),
+          const SizedBox(height: 20),
+          Center(
+            child: SizedBox(
+                height: hight * .50,
+                width: width * .90,
+                child: Image.asset(
+                    'assets/images/Digital transformation-bro.png')),
+          ),
           const Spacer(),
           InkWell(
             onTap: () {
-              Get.to(() => LoginPage());
+              Get.to(() => const LoginPage());
             },
             child: Container(
               height: 50,
